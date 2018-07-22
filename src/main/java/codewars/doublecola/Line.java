cc3e43b58ce85d45fdb56dab0fef00d7a703e43d
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-22 23:13:07 +0200

    Double Cola

---
+++ src/main/java/codewars/doublecola/Line.java
@@ -0,0 +1,9 @@
+package codewars.doublecola;
+
+public class Line {
+    public static String WhoIsNext(String[] names, int n)
+    {
+        // Your code is here...
+        return null;
+    }
+}
