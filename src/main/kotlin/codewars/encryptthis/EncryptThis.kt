35f1347b1ba99e788dd17e459ab362801fbace63
kwiecien <k.d.kwiecinski@gmail.com>
2019-05-29 21:58:03 +0200

    codewars: Encrypt this! using more regex

--- src/main/kotlin/codewars/encryptthis/EncryptThis.kt
+++ src/main/kotlin/codewars/encryptthis/EncryptThis.kt
@@ -10,3 +10,23 @@ fun encryptThis(text: String): String {
     }
     return encryptedWords.reduce { acc, s -> "$acc $s" }
 }
+
+fun encryptThisMoreRegEx(text: String): String {
+    return text.split(" ")
+            .joinToString(" ") {
+                convertFirstLetterToAscii(it) + encryptSuffix(it)
+            }
+}
+
+private fun convertFirstLetterToAscii(word: String) = word.first().toInt().toString()
+
+private fun encryptSuffix(word: String): String {
+    val suffix = word.drop(1)
+    return when (suffix.length) {
+        0, 1 -> suffix
+        else -> {
+            val (first, middle, last) = """(^\w)(\w*)(\w$)""".toRegex().find(suffix)!!.destructured
+            "$last$middle$first"
+        }
+    }
+}
--- src/test/kotlin/codewars/encryptthis/EncryptThisTest.kt
+++ src/test/kotlin/codewars/encryptthis/EncryptThisTest.kt
@@ -1,6 +1,7 @@
 package codewars.encryptthis
 
 import encryptThis
+import encryptThisMoreRegEx
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -13,6 +14,16 @@ class EncryptThisTests {
         assertEquals("84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare", encryptThis("The less he spoke the more he heard"))
         assertEquals("87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri", encryptThis("Why can we not all be like that wise old bird"))
         assertEquals("84kanh 121uo 80roti 102ro 97ll 121ruo 104ple", encryptThis("Thank you Piotr for all your help"))
+    }
+
+    @Test
+    fun testMoreRegEx() {
+        // Example Test Cases
+        assertEquals("65 119esi 111dl 111lw 108dvei 105n 97n 111ka", encryptThisMoreRegEx("A wise old owl lived in an oak"))
+        assertEquals("84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp", encryptThisMoreRegEx("The more he saw the less he spoke"))
+        assertEquals("84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare", encryptThisMoreRegEx("The less he spoke the more he heard"))
+        assertEquals("87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri", encryptThisMoreRegEx("Why can we not all be like that wise old bird"))
+        assertEquals("84kanh 121uo 80roti 102ro 97ll 121ruo 104ple", encryptThisMoreRegEx("Thank you Piotr for all your help"))
     }
 
 }
