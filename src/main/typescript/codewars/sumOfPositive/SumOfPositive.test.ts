651fd7459be3d6eed71999aae9313267ca357f9b
kwiecien <k.d.kwiecinski@gmail.com>
2019-06-11 23:05:42 +0200

    codewars: Sum of positive (TypeScript)

---
+++ src/main/typescript/codewars/sumOfPositive/SumOfPositive.test.ts
@@ -0,0 +1,15 @@
+/// <reference path="/runner/typings/mocha/index.d.ts" />
+/// <reference path="/runner/typings/chai/index.d.ts" />
+import solution = require("./solution");
+
+import { assert } from "chai";
+
+describe("solution", function() {
+  it("Basic Tests", function() {
+    assert.equal(solution.positiveSum([1, 2, 3, 4, 5]), 15);
+    assert.equal(solution.positiveSum([1, -2, 3, 4, 5]), 13);
+    assert.equal(solution.positiveSum([]), 0);
+    assert.equal(solution.positiveSum([-1, -2, -3, -4, -5]), 0);
+    assert.equal(solution.positiveSum([-1, 2, 3, 4, -5]), 9);
+  });
+});
