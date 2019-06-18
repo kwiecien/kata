aa7879c84a848784019c376e085fc088bd0f60ca
kwiecien <k.d.kwiecinski@gmail.com>
2019-06-18 22:37:22 +0200

    codewars: Refactor sum of the first nth

--- src/main/typescript/SumOfTheFirstNthTermOfSeries.js
+++ src/main/typescript/SumOfTheFirstNthTermOfSeries.js
@@ -2,13 +2,13 @@
  * @return {string}
  */
 function SeriesSum(n) {
-    return Array.from(Array(n).keys())
-    .map(i => 3 * i + 1)
-    .reduce((acc, v) => acc + 1 / v, 0)
-    .toFixed(2)
-    .toString();
+    return Array.from( // Creates an array from an iterable object. Array.from(arrayLike[, mapFn[, thisArg]])
+        Array(n).keys(), // The keys() method returns a new Array Iterator object that contains the keys for each index in the array
+        i => 3 * i + 1)
+        .reduce((acc, v) => acc + 1 / v, 0)
+        .toFixed(2)
+        .toString();
 }
-
 
 
 function SeriesSum(n) {
